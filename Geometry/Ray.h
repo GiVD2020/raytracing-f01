ba52ebee13cb470e4483adc39c7c43eef136ea7b
Albert Mir <amirmonc7@alumnes.ub.edu>
2021-03-15 17:37:11 +0100

    Fase2A

--- Geometry/Ray.h
+++ Geometry/Ray.h
@@ -1,6 +1,6 @@
 #pragma once
 #include "glm/glm.hpp"
-
+#include "glm/gtc/random.hpp"
 using namespace glm;
 
 class Ray {
--- Renders/RayTracing.h
+++ Renders/RayTracing.h
@@ -5,6 +5,8 @@
 
 class RayTracing
 {
+private:
+    int numSamples = 10;
 protected:
     // Scene: scena virtual dels objectes amb els seus materials i textures i les llums
     shared_ptr<Scene> scene;
