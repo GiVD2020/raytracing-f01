c42f853b1e69459f8748cfe574619fb63f2cc6f8
Carla Morral <carla.morral@gmail.com>
2021-03-20 15:08:00 +0100

    petita correccio acne scatter

--- Geometry/Lambertian.cpp
+++ Geometry/Lambertian.cpp
@@ -18,8 +18,8 @@ Lambertian::~Lambertian()
 }
 
 bool Lambertian::scatter(const Ray& r_in, const HitInfo& rec, vec3& color, std::vector<Ray>& r_out) const  {
-    vec3 newp = r_in.initialPoint() + 0.01f*r_in.dirVector();
-    vec3 target = newp + rec.normal + this->RandomInSphere();
+    vec3 target = rec.p + rec.normal + this->RandomInSphere();
+    vec3 newp = rec.p + 0.01f*(target-rec.p);
     r_out.push_back( Ray(newp, target-newp));
     color = diffuse;
     return true;
