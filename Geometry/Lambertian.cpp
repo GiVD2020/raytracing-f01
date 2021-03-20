4e620b29ed73a76521b97e0f668e898cf5cacf79
Carla Morral <carla.morral@gmail.com>
2021-03-20 15:17:15 +0100

    canvi scatter

--- Geometry/Lambertian.cpp
+++ Geometry/Lambertian.cpp
@@ -18,9 +18,9 @@ Lambertian::~Lambertian()
 }
 
 bool Lambertian::scatter(const Ray& r_in, const HitInfo& rec, vec3& color, std::vector<Ray>& r_out) const  {
-    vec3 target = rec.p + rec.normal + this->RandomInSphere();
-    vec3 newp = rec.p + 0.01f*(target-rec.p);
-    r_out.push_back( Ray(newp, target-newp));
+    vec3 target = rec.normal + this->RandomInSphere();
+    vec3 newp = rec.p + 0.01f*target;
+    r_out.push_back( Ray(newp, target));
     color = diffuse;
     return true;
 }
